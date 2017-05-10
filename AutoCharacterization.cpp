3a36a1102fa05e4fbffdec6d83f6b61840ea8a1b
Adrien Perkins <adrienp@stanford.edu>
2017-05-10 15:05:21 -0700

    first commit - mostly complete auto characterization library class

---
+++ AutoCharacterization.cpp
@@ -0,0 +1,237 @@
+#include "AutoCharacterization.h"
+
+AutoCharacterization::AutoCharacterization(Type type, Mode mode, RFPowerMonitor* powerMonitor, AngleStepper* azStepper, AngleStepper* elStepper) :
+_type(type),
+_mode(mode),
+_powerMonitor(powerMonitor),
+_azStepper(azStepper),
+_elStepper(elStepper),
+_numMeasurements(5),
+_frequency(10),
+_minAzAngle(0),
+_maxAzAngle(359),
+_minElAngle(0),
+_maxElAngle(90),
+_lastMeasurementTime(0),
+_measurementCount(0),
+_azCompleted(false),
+_elCompleted(false),
+_completed(false)
+{
+	// calculate the timeout time
+	_timeout = (unsigned long) (1.0/_frequency * 1000.0);
+
+}
+
+AutoCharacterization::AutoCharacterization(Type type, Mode mode, RFPowerMonitor* powerMonitor, AngleStepper* stepper) : 
+AutoCharacterization(type, mode, powerMonitor, nullptr, nullptr)
+{
+	switch (type) {
+		case Type::FULL:
+			// TODO: need to throw an error here
+			break;
+
+		case Type::AZIMUTH:
+			_azStepper = stepper;
+			break;
+
+		case Type::ELEVATION:
+			_elStepper = stepper;
+			break;
+	}
+}
+
+
+void AutoCharacterization::setNumMeasurements(int numMeasurements) {
+	_numMeasurements = numMeasurements;
+}
+
+
+void AutoCharacterization::setFrequency(int frequency) {
+	_frequency = frequency;
+}
+
+
+void AutoCharacterization::setAzimuthStepIncrement(int numSteps) {
+	if (_azStepper != nullptr) {
+		_azStepper->setNextStepSize(numSteps);
+	}
+}
+
+
+void AutoCharacterization::setElevationStepIncrement(int numSteps) {
+	if (_elStepper != nullptr) {
+		_elStepper->setNextStepSize(numSteps);
+	}
+}
+
+
+void AutoCharacterization::setAzimuthSweep(int minAngle, int maxAngle) {
+	_minAzAngle = minAngle;
+	_maxAzAngle = maxAngle;
+}
+
+
+void AutoCharacterization::setElevationSweep(int minAngle, int maxAngle) {
+	_minElAngle = minAngle;
+	_maxElAngle = maxAngle;
+}
+
+
+bool AutoCharacterization::isCompleted() {
+	return _completed;
+}
+
+
+void AutoCharacterization::run() {
+
+	// want to do this at a specific frequency
+	if (millis() - _lastMeasurementTime < _timeout) {
+		return;
+	}
+	
+
+	// move motors, only if have made enough measurements
+	// TODO: move to class
+	if (_measurementCount >= _numMeasurements) {
+		// reset the measurement count flag
+		_measurementCount = 0;
+
+		// motor movement is based on which characterization is being run
+		switch(_type) {
+
+			/* sweeping azimuth and elevation */
+			case Type::FULL:
+				setAzimuth();
+				setElevation();
+
+				// update completed status
+				_completed = (_elCompleted && _azCompleted);
+			break;
+
+			/* sweeping only azimuth */
+			case Type::AZIMUTH:
+				setAzimuth();
+
+				// update completed status
+				_completed = _azCompleted;
+			break;
+
+			/* sweeping only elevation */
+			case Type::ELEVATION:
+				setElevation();
+
+				// update completed status
+				_completed = _elCompleted;
+			break;
+		}
+
+		// end the run if the characterization is completed
+		if (_completed) {
+			sendStatus(Status::FINISHED);
+			return;
+		}
+	}
+
+	// make a single measurement
+	float measurement = _powerMonitor->makeMeasurement();
+	_measurementCount++;
+	_lastMeasurementTime = millis();
+
+	// send the data to the computer
+	sendMeasurement(measurement);
+}
+
+
+void AutoCharacterization::setAzimuth() {
+	if (_azStepper == nullptr) {
+		// TODO: need to throw some kind of error
+		return;
+	}
+
+	// if still have azimuth to sweep out, move to the next step
+	if (_azStepper->getAngleSwept() < _maxAzAngle) {
+		_azStepper->moveToNext();
+
+	} else {  // once we've swept through the entire azimuth range, reset
+		_azStepper->moveTo(_minAzAngle);
+		_azStepper->resetAngleSwept();
+		_azCompleted = true;
+	}
+}
+
+
+void AutoCharacterization::setElevation() {
+	if (_elStepper == nullptr) {
+		// TODO: need to throw some kind of error
+		return;
+	}
+
+	// only move elevaiton motor when the azimuth is completed
+	if (!_azCompleted) {
+		return;
+	}
+
+	// if still have elevation to sweep out, move to the next step
+	if (_elStepper->getAngleSwept() < _maxElAngle) {
+		_elStepper->moveToNext();
+		_azCompleted = false;
+
+	} else {  // once we've swept through the entire elevation range, reset
+		_elStepper->moveTo(_minElAngle);
+		_elStepper->resetAngleSwept();
+		_elCompleted = true;
+	}
+}
+
+
+void AutoCharacterization::sendMeasurement(float measurement) {
+
+	float currentAzAngle = 0;
+	if (_azStepper != nullptr) {
+		currentAzAngle = _azStepper->getCurrentAngle();
+	}
+
+	float currentElAngle = 0;
+	if (_elStepper != nullptr) {
+		currentElAngle = _elStepper->getCurrentAngle();
+	}
+
+	// pack the message
+	MeasurementMessage msg;
+	msg.timestamp = _lastMeasurementTime;
+	msg.signalStrength = measurement;
+	msg.azimuth = currentAzAngle;
+	msg.elevation = currentElAngle;
+
+
+	// send the data over the serial port
+	// sync bytes first
+	Serial.write(SYNC_1);
+	Serial.write(SYNC_2);
+
+	// the actual data
+	Serial.write((uint8_t*) &msg, sizeof(msg));
+
+	return;
+}
+
+
+void AutoCharacterization::sendStatus(Status status) {
+
+	// pack the message
+	StatusMessage msg;
+	msg.timestamp = millis();
+	msg.status = static_cast<uint8_t>(status);
+
+	// send the data over the serial port
+	// sync bytes first
+	Serial.write(SYNC_1);
+	Serial.write(SYNC_2);
+
+	// the actual data
+	Serial.write((uint8_t*) &msg, sizeof(msg));
+
+	return;
+
+}
---
+++ AutoCharacterization.h
@@ -0,0 +1,131 @@
+/**
+ * AutoCharacterization.h
+ *
+ * Class to control an automated characterization run.
+ * 
+ */
+
+#ifndef AutoCharacterization_h
+#define AutoCharacterization_h
+
+#include <Arduino.h>
+#include <AngleStepper.h>
+#include <RFPowerMonitor.h>
+
+class AutoCharacterization {
+
+public:
+
+	/**
+	 * the type of characterization to run (which axes to sweep)
+	 */
+	enum class Type : uint8_t {
+		FULL = 0,
+		AZIMUTH,
+		ELEVATION
+	};
+
+	/**
+	 * the mode the characterization is running in.
+	 *
+	 * AZ_EL - defining the platforms as azimuth and elevation (antenna mounted horizontally)
+	 * PHI_THETA - defining the platforms as phi and theta (antenna mounted vertically)
+	 */
+	enum class Mode : uint8_t {
+		AZ_EL = 0,
+		PHI_THETA,
+	};
+
+	/**
+	 * the current status of the characterization run.
+	 */
+	enum class Status : uint8_t {
+		RUNNING = 0,
+		PAUSED,
+		FINISHED,
+	};
+
+
+
+	/**
+	 * Constructor
+	 */
+	AutoCharacterization(Type type, Mode mode, RFPowerMonitor* powerMonitor, AngleStepper* azStepper, AngleStepper* elStepper);
+	AutoCharacterization(Type type, Mode mode, RFPowerMonitor* powerMonitor, AngleStepper* stepper);
+
+	void setNumMeasurements(int numMeasurements);
+	void setFrequency(int frequency);
+
+	void setAzimuthStepIncrement(int numSteps);
+	void setElevationStepIncrement(int numSteps);
+
+	void setAzimuthSweep(int minAngle, int maxAngle);
+	void setElevationSweep(int minAngle, int maxAngle);
+
+	bool isCompleted();
+
+	void run();
+
+
+private:
+
+	/** the characterization run type */
+	Type _type;
+
+	Mode _mode;
+
+	RFPowerMonitor* _powerMonitor;
+
+	AngleStepper* _azStepper;
+	AngleStepper* _elStepper;
+
+	int _numMeasurements;
+	int _frequency;
+	unsigned long _timeout;
+
+	int _minAzAngle;
+	int _maxAzAngle;
+
+	int _minElAngle;
+	int _maxElAngle;
+
+	unsigned long _lastMeasurementTime;
+	int _measurementCount;
+	bool _azCompleted;
+	bool _elCompleted;
+	bool _completed;
+
+	static const byte SYNC_1 = 0xA0;
+	static const byte SYNC_2 = 0xB1;
+
+	struct __attribute__((__packed__)) MeasurementMessage {
+	  unsigned long timestamp;
+	  float signalStrength;
+	  float azimuth;
+	  float elevation;
+	};
+
+	struct __attribute__((__packed__)) StatusMessage {
+		unsigned long timestamp;
+		uint8_t status;
+	};
+
+
+	void setAzimuth();
+	void setElevation();
+
+
+	void sendMeasurement(float measurement);
+
+	void sendStatus(Status status);
+
+
+
+
+};
+
+
+
+
+
+#endif /* AutoCharacterization_h */
